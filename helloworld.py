0b640f948db092530a7414b466206d49519b3f27
Samuel Rincon <samrncn@gmail.com>
2023-05-19 11:02:53 -0700

    fix merge

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-def hello_world(city,state):
-    print(f'Hello World {city},{state}!')
+def hello_world(city, state):
+    print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
-=======
-def hello_world(city):
-    print(f'Hello World{city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> 74fee3b306cf2a23eb87ddb74ccc1af1082d7995
 
 
 main()
